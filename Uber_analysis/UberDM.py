bd71226d70c0d5c899cfd733a437886b1bd5bfb8
user <mybestzoo@gmail.com>
2018-05-07 08:12:58 -0700

    Prophet test

--- Uber_analysis/UberDM.py
+++ Uber_analysis/UberDM.py
@@ -11,10 +11,16 @@ import matplotlib.pyplot as plt
 
 
 # Read data
-df = pd.read_csv("uber_DM_3h.csv")
+df = pd.read_csv("uberMoscow.csv")
 
-df_uberX = df[df.product_type == 'uberKIDS']
+df_uberX = df[df.start_location_id == 1]
+df_uberX = df_uberX[df.product_type == 'uberX']
 df_uberX = df_uberX.reset_index()
+df_uberX.convert_objects(convert_numeric=True)
+df_uberX['ETAMean'] = df_uberX['expected_wait_time'].rolling(50).mean()
 
 # plot surge_multiplier over time
-plt.plot(df_uberX['surge_multiplier'])
+plt.plot(df_uberX['surge_multiplier'],color='blue')
+plt.plot(df_uberX['expected_wait_time'].convert_objects(convert_numeric=True)/300,color='yellow')
+plt.plot(df_uberX['ETAMean']/300,color='green')
+plt.show()
